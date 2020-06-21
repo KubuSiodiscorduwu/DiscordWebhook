c2ae766b4ce9e09cd62b19fd96af14def0d9e778
KubuSiodiscorduwu <66824908+KubuSiodiscorduwu@users.noreply.github.com>
2020-06-21 13:55:54 +0200

    Update page.inc.php

--- examples/send-form/inc/page.inc.php
+++ examples/send-form/inc/page.inc.php
@@ -10,8 +10,8 @@
 </header>
 <main>
   <form action="index.php" method="POST">
-    <label for="username">Username:</label>
-    <input id="username" type="text" name="username" placeholder="merlin#2222">
+    <label for="username">Nick Discord z tagiem:</label>
+    <input id="username" type="text" name="username" placeholder="KubuS#6171">
     <br>
 
     <label for="username">Wybierz rangę:</label>
@@ -23,9 +23,18 @@
       <option value="admin">Administrator</option>
     </select>
     <br>
+
+    <label for="description">Ile masz lat</label><br>
+    <textarea name="wiek" id="description" cols="5" rows="3">
+    </textarea>
+    <br>
     <label for="description">Dlaczego chcesz mieć rangę na naszym serwerze Discord?</label><br>
     <textarea name="description" id="description" cols="30" rows="10">
 
+    </textarea>
+    <br>
+    <label for="description">Coś o tobie:</label><br>
+    <textarea name="cot" id="description" cols="30" rows="10">
     </textarea>
     <br>
     <input type="submit" name="action" value="Wyślij">
@@ -33,7 +42,7 @@
   </form>
 </main>
 <footer>
-  &copy; 2019 — Marcin Stawowczyk
+  &copy; 2020 — KubuS
 </footer>
 
 </body>
